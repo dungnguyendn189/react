f9455d14652c78e9bdf54bfde27dc0a1477fb352
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-10-04 16:59:36 +0700

    Update Source

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,5 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './ui/home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/menu',
+    element: <Menu />
+  },
+  { path: '/cart', element: <Cart /> },
+  { path: '/older/new', element: <CreateOrder /> }
+]);
+
 function App() {
-  const x = 23;
-  return <div>Hello Vite!</div>
+  return <RouterProvider router={router} />;
 }
 export default App;
