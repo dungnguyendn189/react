bc554873c2eee316cbb1ac142acfdd34866b6e01
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-12-11 16:28:57 +0700

    update source

--- the-wild-oasis/src/ui/Select.jsx
+++ the-wild-oasis/src/ui/Select.jsx
@@ -14,6 +14,6 @@ const StyledSelect = styled.select`
   box-shadow: var(--shadow-sm);
 `;
 
-function Select() {
-  return <div></div>;
+function Select({ options, value }) {
+  return <StyledSelect value={value}></StyledSelect>;
 }
