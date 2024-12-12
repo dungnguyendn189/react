5f6379e3a0778f088449c104c232e7dbd97dd726
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-12-12 14:02:49 +0700

    sorby

--- the-wild-oasis/src/ui/Select.jsx
+++ the-wild-oasis/src/ui/Select.jsx
@@ -14,6 +14,16 @@ const StyledSelect = styled.select`
   box-shadow: var(--shadow-sm);
 `;
 
-function Select({ options, value }) {
-  return <StyledSelect value={value}></StyledSelect>;
+function Select({ options, value, onChange, ...props }) {
+  return (
+    <StyledSelect value={value} onChange={onChange} {...props}>
+      {options.map((option) => (
+        <option value={option.value} key={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  );
 }
+
+export default Select;
--- the-wild-oasis/src/ui/SortBy.jsx
+++ the-wild-oasis/src/ui/SortBy.jsx
@@ -1,5 +1,15 @@
+import { useSearchParams } from "react-router-dom";
+import Select from "./Select";
+
 function SortBy({ options }) {
-  return <div>Sort</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function handleChange(e) {
+    console.log(e.target.value);
+    searchParams.set("sortBy", e.target.value);
+    setSearchParams(searchParams);
+  }
+  return <Select options={options} type="white" onChange={handleChange} />;
 }
 
 export default SortBy;
