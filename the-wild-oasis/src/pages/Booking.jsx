fcb06a1aeb66b3080516f57191040a6a6d821e7e
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-12-24 10:46:20 +0700

    up date detail

---
+++ the-wild-oasis/src/pages/Booking.jsx
@@ -0,0 +1,11 @@
+import BookingDetail from "../features/bookings/BookingDetail";
+
+function Booking() {
+  return (
+    <return>
+      <BookingDetail />
+    </return>
+  );
+}
+
+export default Booking;
