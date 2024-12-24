c30608dd836ca0cdf75d0c371f4815f7c0b7569e
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-12-24 17:02:11 +0700

    up source

--- the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -1,22 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-import Spinner from "ui/Spinner";
-import BookingDataBox from "./BookingDataBox";
-import Row from "ui/Row";
-import Heading from "ui/Heading";
-import Tag from "ui/Tag";
-import ButtonGroup from "ui/ButtonGroup";
-import Button from "ui/Button";
-import Modal from "ui/Modal";
-import ConfirmDelete from "ui/ConfirmDelete";
+import Row from "../../ui/Row";
+import Heading from "../../ui/Heading";
+import ButtonGroup from "../../ui/ButtonGroup";
+import Button from "../../ui/Button";
 
-import { useBooking } from "../features/bookings/useBooking";
-import { useDeleteBooking } from "../useDeleteBooking";
-import { useMoveBack } from "../hooks/useMoveBack";
-import { useCheckout } from "../features/check-in-out/useCheckout";
-import ButtonText from "../ui/ButtonText";
-import Empty from "ui/Empty";
+import { useMoveBack } from "../../hooks/useMoveBack";
+import ButtonText from "../../ui/ButtonText";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -25,15 +16,8 @@ const HeadingGroup = styled.div`
 `;
 
 function BookingDetail() {
-  const { booking } = useBooking();
-  const { mutate: deleteBooking, isLoading: isDeleting } = useDeleteBooking();
-  const { mutate: checkout, isLoading: isCheckingOut } = useCheckout();
-
   const moveBack = useMoveBack();
   const navigate = useNavigate();
-
-  // if (isLoading) return <Spinner />;
-  // if (!booking) return <Empty resource='booking' />;
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -41,34 +25,42 @@ function BookingDetail() {
     "checked-out": "silver",
   };
 
-  const { id: bookingId, status } = booking;
-
-  // We return a fragment so that these elements fit into the page's layout
   return (
     <>
       <Row type="horizontal">
         <HeadingGroup>
-          <Heading type="h1">Booking #{bookingId}</Heading>
-          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
+          <Heading type="h1">Booking #Aa</Heading>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
-
-      <BookingDataBox booking={booking} />
-
       <ButtonGroup>
-        {status === "unconfirmed" && (
+        <Button variation="secondary" onClick={moveBack}>
+          Back
+        </Button>
+      </ButtonGroup>
+    </>
+  );
+}
+
+export default BookingDetail;
+
+{
+  /* {status === "unconfirmed" && (
           <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
             Check in
           </Button>
-        )}
+        )} */
+}
 
-        {status === "checked-in" && (
+{
+  /* {status === "checked-in" && (
           <Button onClick={() => checkout(bookingId)} disabled={isCheckingOut}>
             Check out
           </Button>
-        )}
-
+        )} */
+}
+{
+  /* 
         <Modal>
           <Modal.Toggle opens="delete">
             <Button variation="danger">Delete booking</Button>
@@ -81,14 +73,5 @@ function BookingDetail() {
               disabled={isDeleting}
             />
           </Modal.Window>
-        </Modal>
-
-        <Button variation="secondary" onClick={moveBack}>
-          Back
-        </Button>
-      </ButtonGroup>
-    </>
-  );
+        </Modal> */
 }
-
-export default BookingDetail;
