64d1423036fab379d5d64337c2a742337522a6e5
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2024-12-31 15:26:00 +0700

    update win

--- the-wild-oasis/src/features/check-in-out/CheckinBooking.jsx
+++ the-wild-oasis/src/features/check-in-out/CheckinBooking.jsx
@@ -6,14 +6,23 @@ import styled from "styled-components";
 import BookingDataBox from "../bookings/BookingDataBox";
 import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
+import { useBooking } from "../bookings/useBooking";
+import Snipper from "../../ui/Spinner";
+import { useEffect, useState } from "react";
+import CheckBox from "../../ui/Checkbox";
 
 const Box = styled.div`
   padding: 2.4rem 4rem;
 `;
 
 function CheckinBooking() {
+  const [confirmPaid, setComfirmPaid] = useState(false);
+  const { booking, isLoading } = useBooking();
+
+  useEffect(() => setComfirmPaid(booking?.isPaid || false), [booking]);
+
   const moveBack = useMoveBack();
-  const booking = {};
+  if (isLoading) return <Snipper />;
 
   const {
     id: bookingId,
@@ -31,7 +40,18 @@ function CheckinBooking() {
         <Heading type="h1">Check in booking #{bookingId}</Heading>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
-      {/* <BookingDataBox booking={booking} /> */}
+      <BookingDataBox booking={booking} />
+      <Box>
+        <CheckBox
+          checked={confirmPaid}
+          onChange={() => setComfirmPaid((comfirm) => comfirm)}
+          disabled={confirmPaid}
+          id="comfirm"
+        >
+          {" "}
+          I confirm that {guests.fullName} has paid the total amount
+        </CheckBox>
+      </Box>
       <ButtonGroup>
         <Button onClick={handleCheckin}>Checkin #Booking</Button>
         <Button onClick={moveBack} variation="secondary">
