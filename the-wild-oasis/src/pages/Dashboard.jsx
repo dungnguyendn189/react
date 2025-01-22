450175d37401f1a8e692928b20e4a77e23b89558
dungnguyendn189 <84318162+dungnguyendn189@users.noreply.github.com>
2025-01-22 11:30:36 +0700

    filter dashboard

--- the-wild-oasis/src/pages/Dashboard.jsx
+++ the-wild-oasis/src/pages/Dashboard.jsx
@@ -1,12 +1,19 @@
+import DashboardFilter from "../features/dashboard/DashboardFilter";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+import { useRecentBookings } from "../features/dashboard/useRecentBookings";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Dashboard() {
+  const { bookings, isLoading } = useRecentBookings();
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+      <DashboardLayout />
+    </>
   );
 }
 
